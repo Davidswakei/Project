1e2f8252f3d75c353edea37e0143f68b5d6dfa01
Davidswakei <99643100+Davidswakei@users.noreply.github.com>
2022-07-01 14:49:01 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,16 @@
+/*
+simple calculator
+created by David Marwa
+on July, 2022
+C89 compiler
+MIT licence
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("\tsimple calculator!\n");
+    return 0;
+}
